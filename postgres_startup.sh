5d1e920cb43753611fdf90956d88f852552b7eca
Paulo Diniz <pdiniz_100@yahoo.com>
2017-02-03 15:52:49 -0800

    add assume-yes

--- postgres_startup.sh
+++ postgres_startup.sh
@@ -39,9 +39,9 @@ echo "" >> /usr/local/startup.log
 # update package lists for PostgreSQL 9.6
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-sudo apt-get -qq update
-sudo apt-get -qq -y upgrade
-sudo apt-get install -y jq
+sudo apt-get  --assume-yes -qq update
+sudo apt-get  --assume-yes -qq upgrade
+sudo apt-get  --assume-yes -qq install jq
 echo "apt-get update exited with: $?"
 echo "updated packages" >> /usr/local/startup.log
 echo "" >> /usr/local/startup.log
