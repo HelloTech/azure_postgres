b9864c5596a6c22c20a9bde93174913d5c6253d1
Paulo Diniz <pdiniz_100@yahoo.com>
2017-02-03 22:58:55 -0800

    test with export DEBIAN_FRONTEND=noninteractive

--- postgres_startup.sh
+++ postgres_startup.sh
@@ -39,7 +39,8 @@ echo "" >> /usr/local/startup.log
 # update package lists for PostgreSQL 9.6
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-sudo apt-get  --assume-yes -qq install jq
+sudo apt-get -qq update
+sudo apt-get --assume-yes -qq install jq
 echo "apt-get update exited with: $?"
 echo "updated packages" >> /usr/local/startup.log
 echo "" >> /usr/local/startup.log
